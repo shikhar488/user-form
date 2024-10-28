68790d0dc81d0f9a7700cf721e386c29d84f7b42
Shikhar Joshi <46199723+shikhar488@users.noreply.github.com>
2024-10-28 08:17:42 +0530

    added more functionalities

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -3,13 +3,17 @@ import { useLocation } from "react-router-dom";
 function UserDetails(){
     
         const location = useLocation();
-    const { name, password } = location.state || {}; 
+    const { name, password, email } = location.state || {}; 
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid = emailPattern.test(email);
 
     return (
         <div>
             <h1>Received Data</h1>
             <p>Name: {name}</p>
             <p>Password: {password}</p>
+            {isValid ? <p>Email: {email}</p> : <p>Invalid Email</p>}
         </div>
     );
     
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -5,11 +5,12 @@ function UserInput(){
 
     const [name,setName] = useState('');
     const [password,setPassword] = useState('');
+    const [email,setEmail] = useState('');
     const navigate = useNavigate();
 
     function handleOnSubmit(e){
         e.preventDefault();
-        navigate("/userDetails",{state:{name:name,password:password}})
+        navigate("/userDetails",{state:{name:name,password:password,email:email}})
     }
 
     return(
@@ -21,13 +22,51 @@ function UserInput(){
             />
             <label>Enter your name here</label>
             <br/>
+
+            <input type="email"
+            placeholder="enter ur email" 
+             value={email}
+             onChange={(e)=>setEmail(e.target.value)}
+             required
+            />
+            <label>Enter your email here</label>
+            <br/>
+
             <input type="password"
             placeholder="enter ur password" 
             value={password}
             onChange={(e)=>setPassword(e.target.value)}
+            required
             />
             <label>Enter your password here</label>
             <br/>
+
+            <input type="date" required/>
+            <br/>
+
+            <input type="file" required/>
+            <br/>
+
+            <fieldset>
+                <legend>Choose from the following: </legend>
+
+                <div>
+                    <input type="checkbox" id="apple" checked />
+                    <label htmlFor="apple">Apples</label>
+                </div>
+
+                <div>
+                    <input type="checkbox" id="banana" />
+                    <label htmlFor="banana">Banana</label>
+                </div>
+
+                <div>
+                    <input type="checkbox" id="grapes" />
+                    <label htmlFor="grapes">Grapes</label>
+                </div>
+            </fieldset>
+
+
             <button type="submit">Submit the data</button>
         </form>
     )
