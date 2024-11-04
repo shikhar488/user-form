d98df23831b2d281e56f94ed6b4f545f6c84a51c
Shikhar Joshi <46199723+shikhar488@users.noreply.github.com>
2024-11-04 23:22:40 +0530

    added the additional features

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 function UserDetails(){
     
         const location = useLocation();
-    const { name, password, email } = location.state || {}; 
+    const { name, password, email, date, selectedOptions } = location.state || {}; 
 
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const isValid = emailPattern.test(email);
@@ -14,6 +14,12 @@ function UserDetails(){
             <p>Name: {name}</p>
             <p>Password: {password}</p>
             {isValid ? <p>Email: {email}</p> : <p>Invalid Email</p>}
+            {date && <p>Selected Date: {date.toDateString()}</p>}
+            <ul>
+                {selectedOptions.map(([option, isSelected]) => (
+                    isSelected && <li key={option}>{option}</li>
+                ))}
+            </ul>
         </div>
     );
     
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,80 +1,117 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
 
-function UserInput(){
+function UserInput() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [date, setDate] = useState(new Date());
+    const [selectedOptions, setSelectedOptions] = useState({
+        apple: true,
+        banana: false,
+        grapes: false,
+    });
 
-    const [name,setName] = useState('');
-    const [password,setPassword] = useState('');
-    const [email,setEmail] = useState('');
     const navigate = useNavigate();
 
-    function handleOnSubmit(e){
-        e.preventDefault();
-        navigate("/userDetails",{state:{name:name,password:password,email:email}})
+    function handleCheckboxChange(e) {
+        const { id, checked } = e.target;
+        setSelectedOptions((prevState) => ({
+            ...prevState,
+            [id]: checked,
+        }));
     }
 
-    return(
+    function handleOnSubmit(e) {
+        e.preventDefault();
+        navigate("/userDetails", {
+            state: {
+                name: name,
+                password: password,
+                email: email,
+                date: date,
+                selectedOptions: selectedOptions,
+            },
+        });
+    }
+
+    return (
         <>
-        <form onSubmit={handleOnSubmit}>
-            <input type="text"
-            placeholder="enter ur name" 
-             value={name}
-             onChange={(e)=>setName(e.target.value)}
-            />
-            <label>Enter your name here</label>
-            <br/>
+            <form onSubmit={handleOnSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <label>Enter your name here</label>
+                <br />
 
-            <input type="email"
-            placeholder="enter ur email" 
-             value={email}
-             onChange={(e)=>setEmail(e.target.value)}
-             required
-            />
-            <label>Enter your email here</label>
-            <br/>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <label>Enter your email here</label>
+                <br />
 
-            <input type="password"
-            placeholder="enter ur password" 
-            value={password}
-            onChange={(e)=>setPassword(e.target.value)}
-            required
-            />
-            <label>Enter your password here</label>
-            <br/>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <label>Enter your password here</label>
+                <br />
 
-            <input type="date" required/>
-            <br/>
+                <DatePicker selected={date} onChange={(date) => setDate(date)} />
+                <br />
 
-            <input type="file" required/>
-            <br/>
+                <input type="file" required />
+                <br />
 
-            <fieldset>
-                <legend>Choose from the following: </legend>
+                <fieldset>
+                    <legend>Choose from the following:</legend>
 
-                <div>
-                    <input type="checkbox" id="apple" checked />
-                    <label htmlFor="apple">Apples</label>
-                </div>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="apple"
+                            checked={selectedOptions.apple}
+                            onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="apple">Apples</label>
+                    </div>
 
-                <div>
-                    <input type="checkbox" id="banana" />
-                    <label htmlFor="banana">Banana</label>
-                </div>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="banana"
+                            checked={selectedOptions.banana}
+                            onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="banana">Banana</label>
+                    </div>
 
-                <div>
-                    <input type="checkbox" id="grapes" />
-                    <label htmlFor="grapes">Grapes</label>
-                </div>
-            </fieldset>
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="grapes"
+                            checked={selectedOptions.grapes}
+                            onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="grapes">Grapes</label>
+                    </div>
+                </fieldset>
 
-
-            <button type="submit">Submit the data</button>
-        </form>
-
-
-        
+                <button type="submit">Submit the data</button>
+            </form>
         </>
-    )
+    );
 }
 
 export default UserInput;
